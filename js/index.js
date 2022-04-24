8d90311913d9e56154e1442f8e494725e3e18797
Isaac Salirrosas <isaacsalirrosasc@gmail.com>
2022-04-23 23:37:06 -0500

    Assignments portal page created

---
+++ js/index.js
@@ -0,0 +1,37 @@
+const lessons = [
+    'Lesson 02',
+    'Lesson 03',
+    'Lesson 04',
+    'Lesson 05',
+    'Lesson 06',
+    'Lesson 07',
+    'Lesson 08',
+    'Lesson 09',
+    'Lesson 10',
+    'Lesson 11'
+];
+
+const rootList = document.querySelector('.lessons-list');
+
+lessons.forEach((item, idx) => {
+    const listItem = document.createElement('li');
+    const link = document.createElement('a');
+    link.className = `lesson-${idx+2}-link`;
+    link.href = '#';
+    link.textContent = 'Title';
+    listItem.textContent = `${item}: `;
+    listItem.appendChild(link);
+    listItem.className = `lesson-${idx+2}-item lesson-item`;
+    rootList.appendChild(listItem);
+});
+
+const date = new Date().toLocaleString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric'
+});
+
+const dateField = document.querySelector('.date-time');
+dateField.textContent = date;
