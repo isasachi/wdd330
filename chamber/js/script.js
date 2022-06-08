b3b7e4887734278d5b9e9f7ccf6304ee1e246a35
Isaac Salirrosas <isaacsalirrosasc@gmail.com>
2022-06-07 19:20:07 -0500

    Discover page files added

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -31,3 +31,28 @@ function toggleMenu() {
 
 const burgerButton = document.getElementById('burgerBtn');
 burgerButton.onclick = toggleMenu;
+
+const loadImage = (entry) => {
+    const image = entry.target;
+    const src = image.getAttribute('data-src');
+    image.setAttribute('src', src);
+    image.removeAttribute('data-src');
+}
+
+const options = {
+    treshold: 0
+};
+
+const images = document.querySelectorAll('img[data-src]');
+const imageObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            loadImage(entry);
+            observer.unobserve(entry.target);
+        }
+    })
+}, options);
+
+images.forEach(image => {
+    imageObserver.observe(image);
+});
