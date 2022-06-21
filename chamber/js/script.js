b3e7a67e65cc56e975c275369b4ba586c3b7c555
Isaac Salirrosas <isaacsalirrosasc@gmail.com>
2022-06-21 18:25:02 -0500

    Added Directory page to the Chamber website

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -23,14 +23,17 @@ const lastModificationDate = date.toLocaleString('en-US', {
 
 lastModification.textContent = lastModificationDate;
 
+/* Hamburger Menu Button */
+
 function toggleMenu() {
     document.getElementById('primaryNav').classList.toggle('open');
     document.getElementById('burgerBtn').classList.toggle('open');
 }
 
-
 const burgerButton = document.getElementById('burgerBtn');
 burgerButton.onclick = toggleMenu;
+
+/* Lazy Loading */
 
 const loadImage = (entry) => {
     const image = entry.target;
@@ -57,4 +60,42 @@ images.forEach(image => {
     imageObserver.observe(image);
 });
 
-document.querySelector('input[hidden]').value = date;
+/* Comapanies Directory JSON */
+
+const directoryContainer = document.querySelector('.directory-main');
+const directoryURL = 'json/data.json';
+
+async function getCompanies() {
+    const response = await fetch(directoryURL);
+    const responseJSON =  await response.json();
+    responseJSON.forEach(item => {
+        renderCompany(item);
+    })
+}
+
+function renderCompany(company) {
+    const container = document.createElement('div');
+    const companyName = document.createElement('h3');
+    const companyAddress = document.createElement('p');
+    const companyPhoneNumber = document.createElement('p');
+    const companyWebsite = document.createElement('a');
+    const companyLogo = document.createElement('img');
+    const companyArea = document.createElement('p');
+    const companyMembershipLevel = document.createElement('p');
+
+    companyName.textContent = `${company.name}`;
+    companyAddress.textContent = `Address: ${company.address}`;
+    companyPhoneNumber.textContent = `Phone number: ${company.phoneNumber}`;
+    companyWebsite.href = company.websiteURL;
+    companyWebsite.textContent = 'Visit website';
+    companyLogo.src = company.logoImage;
+    companyLogo.alt = company.name;
+    companyArea.textContent = `Area: ${company.area}`;
+    companyMembershipLevel.textContent = `Membership level: ${company.membershipLevel}`
+
+    container.append(companyLogo, companyName, companyAddress, companyPhoneNumber, companyArea, companyMembershipLevel, companyWebsite);
+
+    directoryContainer.append(container);
+}
+
+window.onload = getCompanies();
